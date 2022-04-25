2a737cd469ac8e79af3082e87c1f13a19375ae57
derek quinn <quinnderek@yahoo.com>
2022-04-24 22:39:47 -0400

    Configuring Apollo SDK

---
+++ starter/RocketReserver/Network.swift
@@ -0,0 +1,10 @@
+import Foundation
+import Apollo
+
+class Network {
+
+  static let shared = Network()
+
+  private(set) lazy var apollo = ApolloClient(url: URL(string: "https://apollo-fullstack-tutorial.herokuapp.com/graphql")!)
+
+}
